4cd1d2db4803ad89feb74100d85ecff80c6ae8d7
Mich Seixas <mcseixas@gmail.com>
2023-10-02 22:42:28 +0200

    set up each game image and defined speed identifiers

--- public/game.js
+++ public/game.js
@@ -35,8 +35,86 @@ function register(event) {
         account.createEmailSession( //then create email session
             event.target.elements['register-email'].value,
             event.target.elements['register-password'].value,
-
-        )
+        ).then(() => { //once we've created the email session, show the game display
+            showDisplay()
+        })
     }).catch(error => console.error(error))
     event.preventDefault() //prevent form from reloding the page
 }
+
+// function login(event){
+
+// }
+
+function showDisplay (){ // when ShowDisplay is called, the element with id "Modal" is hidden.
+    const modalElement = document.getElementById('modal')
+    modalElement.classList.add('hidden') 
+}
+showDisplay ()
+
+
+//Kaboom game
+function startGame (){
+    kaboom({
+        global: true,
+        fullscreen: true,
+        scale: 2
+    })
+
+    //define speed identifiers:
+    const moveSpeed = 120
+    const jumpForce = 360
+    const bigJumpForce = 550
+    let currentJumpForce = jumpForce //la currentJumpForce ya va a iniciar con 360
+    const fallDeath = 400
+    const enemyDeath = 20
+
+
+    //Game Logic:
+    let isJumping = true
+
+
+    loadRoot('https://i.imgur.com/')
+    loadSprite('coin', 'wbKxhcd.png')
+    loadSprite('evil-shroom', 'KPO3fR9.png')
+    loadSprite('brick', 'pogC9x5.png')
+    loadSprite('block', 'M6rwarW.png')
+    loadSprite('mario', 'Wb1qfhK.png')
+    loadSprite('mushroom', '0wMd92p.png')
+    loadSprite('surprise', 'gesQ1KP.png')
+    loadSprite('unboxed', 'bdrLpi6.png')
+    loadSprite('pipe-top-left', 'ReTPiWY.png')
+    loadSprite('pipe-top-right', 'hj2GK4n.png')
+    loadSprite('pipe-bottom-left', 'c1cYSbt.png')
+    loadSprite('pipe-bottom-right', 'nqQ79eI.png')
+    loadSprite('blue-block', 'fVscIbn.png')
+    loadSprite('blue-brick', '3e5YRQd.png')
+    loadSprite('blue-steel', 'gqVoI2b.png')
+    loadSprite('blue-evil-mushroom', 'SvV4ueD.png')
+    loadSprite('blue-surprise', 'RMqCc1G.png')
+
+    screen("game", ({level, score}) => {
+        layers(["bg", "obj", "ui"], "obj")
+
+        const map = [
+            '                                      ',
+            '                                      ',
+            '                                      ',
+            '                                      ',
+            '                                      ',
+            '                                      ',
+            '                                      ',
+            '                                      ',
+            '                                      ',
+            '==============================   ====='
+        ],
+        [
+
+        ]
+    ]
+
+    const levelCfg = 
+    })
+
+}
+startGame ()
